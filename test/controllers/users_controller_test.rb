4be79f60b13bc2cac587cc8b5d6511c5a03f97bb
Asep Saepudin <asep.saepudin@emveep.com>
2018-05-03 22:03:26 +0700

    Add test unit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:andi)
+  end
+
+  test "should get list my friends" do
+    assert_no_difference 'User.count' do
+      post friends_url, params: { email: @user.email }
+    end
+    assert_response :success
+  end
+
+  test "should be connected" do
+    assert_difference '@user.following.count', 1 do
+      post connect_url, params: { friends: [@user.email, "rudi@example.com"] }
+    end
+    assert_response :success
+  end
+
+  test "should be list common friends from between email andi and lisa" do
+    post common_friends_url, params: { friends: [@user.email, "lisa@example.com"] }
+    assert_response :success
+  end
+
+  test "should be subscribe friend by an email" do
+    post subscribe_url, params: { requestor: "andi@example.com", target: "lisa@example.com" }
+    assert_response :success
+  end
+
+  test "should be block friend by an email" do
+    post block_url, params: { requestor: "andi@example.com", target: "lisa@example.com" }
+    assert_response :success
+  end
+
+  test "should be send an email" do
+    post send_email_url, params: { sender: "andi@example.com", text: "hello lisa@example.com" }
+    assert_response :success
+  end
 end
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	user       = users(:andi)
+    other_user = users(:john)
+    @friendship = Friendship.new(follower_id: user.id, followed_id: other_user.id)
+  end
+
+  test "should be valid" do
+    assert @friendship.valid?
+  end
+  
+  test "should require a follower_id" do
+    @friendship.follower_id = nil
+    assert_not @friendship.valid?
+  end
+
+  test "should require a followed_id" do
+    @friendship.followed_id = nil
+    assert_not @friendship.valid?
+  end
 end
